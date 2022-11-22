c172c2ff746791f6d12472dae6a4abc7a0f85c71
Pankaj Chakrabarty <pankajchakrabarty22.com>
2022-11-22 13:25:16 +0200

    update

--- server/index.js
+++ server/index.js
@@ -24,12 +24,25 @@ app.get("/",async function (req,res) {
         res.status(500).json({error: err.message})
     }
 })*/
-app.get("/",async function (req,res) {
+
+
+app.get("/v1data",async function (req,res) {
     try { 
         const connection = await mysql.createConnection(config.db)
-        const [outPut,] = await connection.execute('select * from MonthlyData')
-        if (!outPut) outPut=[] 
-        res.status(200).json(outPut)
+        const [monthlyData,] = await connection.execute('select * from MonthlyData')
+        const [annualData,] = await connection.execute('select * from annualData')
+        if (!monthlyData) monthlyData=[] 
+        if (!annualData) annualData=[] 
+        /*const [result,] = await connection.execute('select * from annualData')
+        if (!result) result=[] 
+        res.status(200).json(result)
+        **/
+       res.json({
+        test: ['my array'],
+        test2: "something else",
+        monthlyData: monthlyData,
+        annualData: annualData
+       });
         
     } catch(err) {
 
--- src/App.js
+++ src/App.js
@@ -3,24 +3,25 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import LinearLineGraphDemo from './LinearLineGraph';
 import MonthlyLineGraphDemo from './MonthlyLineGraph';
-const URL = 'http://localhost:3001/'
+const URL = 'http://localhost:3001/v1data'
 
 
 function App() {
   const [annualData, setannualData] = useState([])
-  const [MonthlyData, setMonthlyData] = useState([])
+  const [monthlyData, setmonthlyData] = useState([])
 
   useEffect (() => {
     axios.get(URL)
     .then((response) => {
-      setannualData(response.data)
-      setMonthlyData(response.data)
+      debugger
+      setannualData(response.data.annualData)
+      setmonthlyData(response.data.monthlyData)
     }).catch (error => {
       alert(error.response.data.error)
     })
   }, [])
 
-debugger
+
   return (
     <div>
       <h3> Temperature Fact</h3>
@@ -33,7 +34,7 @@ debugger
       </ul> */}
      
       <LinearLineGraphDemo myData = {annualData} />
-      <MonthlyLineGraphDemo ownData = {MonthlyData} />
+      <MonthlyLineGraphDemo ownData = {monthlyData} />
     </div>
     
   );
