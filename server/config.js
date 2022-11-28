24e84c329bfcef8cabbe4e9aa1122964ceaef0a1
Vanshika-Saini <74698779+Vanshika-Saini@users.noreply.github.com>
2022-11-28 18:02:54 +0200

    v1 data visualization

--- server/config.js
+++ server/config.js
@@ -1,3 +1,4 @@
+//connecting to the database which is created in phpmyadmin, opens when xampp is ran
 const config = {
     db: {
         host: 'localhost',
--- server/index.js
+++ server/index.js
@@ -1,3 +1,7 @@
+//express server is created through which we can connect backend and frontend
+//cors and axios is used to run modules in the project
+//mysql is used to call the database and take the query from database(if any)
+//require is used to connect to database
 const express = require('express')
 const cors = require('cors')
 const mysql = require('mysql2/promise')
@@ -9,9 +13,9 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-const port = 3001
+const port = 3001 //server port which renders data from database
 /*
-app.get("/",async function (req,res) {
+app.get("/",async function (req,res) { 
     try { 
         const connection = await mysql.createConnection(config.db)
         const [result,] = await connection.execute('select * from annualData')
@@ -29,7 +33,7 @@ app.get("/",async function (req,res) {
 app.get("/v1data",async function (req,res) {
     try { 
         const connection = await mysql.createConnection(config.db)
-        const [monthlyData,] = await connection.execute('select * from MonthlyData')
+        const [monthlyData,] = await connection.execute('select * from MonthlyData') // await waits for the process to be done and then shows the output
         const [annualData,] = await connection.execute('select * from annualData')
         const [AnnualNorth,] = await connection.execute('select * from annual_north')
         const [MonthNorth,] = await connection.execute('select * from month_north')
