ae93f7b6a845e5a68c3f737c3c8fcca403cbcc3d
Vanshika-Saini <74698779+Vanshika-Saini@users.noreply.github.com>
2022-12-11 15:25:16 +0200

    User Signup completed.

--- server/react-login.js
+++
@@ -1,9 +0,0 @@
-/*const react_login = {
-    db: {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'react-login'
-    }
-}
-module.exports = react_login;
